cf543943c0c91f72cbb592cec1ccbbeae074aab6
Josh Taylor <joshbtayloriii@gmail.com>
2025-02-09 20:07:08 -0600

    Move parsing to a level parser

--- scenes/level_data.lua
+++ scenes/level_data.lua
@@ -1,9 +1,10 @@
+
+LevelParser = require("scenes/level_parser")
+
 LevelData = Object:extend()
 
-local _MAX_LEVEL_NUMBER = 1
-
 function LevelData:new(levelNumber)
-    local all_data = self:LoadLevel(levelNumber)
+    local all_data = LevelParser.loadLevel(levelNumber)
     self.data = all_data["tiles"]
 
     self.level_objects = LevelObjects(all_data["objects"])
@@ -14,39 +15,6 @@ function LevelData:new(levelNumber)
     self.height = all_data["height"]
 end
 
--- Loads a single tile for a level
--- Adds a floor under objects and adds objects to the "objects" param
--- Returns the tile (floor, pit, wall)
-local function _parseLevelChar(c, objects, charTable, x, y)
-    if c == "b" then
-      objects[x..","..y] = Bone(x, y)
-      return Floor()
-    elseif c == "s" then
-      local character = Character(x, y)
-      charTable["character"] = character
-      objects[x..","..y] = character
-      return Floor()
-    elseif c == "f" then
-      objects[x..","..y] = Fire(x, y)
-      return Floor()
-    elseif c == "." then
-      return Floor()
-    elseif c == "m" then
-      objects[x..","..y] = MetalCrate(x, y)
-      return Floor()
-    elseif c == "w" then
-      objects[x..","..y] = WoodenCrate(x, y)
-      return Floor()
-    elseif c == "x" then
-      return Wall()
-    elseif c == "e" then
-      objects[x..","..y] = Exit(x, y)
-      return Floor()
-    end
-  
-    print("hit bad spot loading level data: "..c)
-    return nil
-end
 
 function LevelData:handlePlayerInput(playerInputDirection)
   local newCharX = self.character.x + playerInputDirection.x
@@ -148,40 +116,6 @@ function LevelData:_canPushHere(x, y)
   local tile = self:getCell(x, y)
   if not tile:canMoveHere() then return false end
 
-end
-
-function LevelData:LoadLevel(levelNumber)
-  if levelNumber < 1 then
-    levelNumber = _MAX_LEVEL_NUMBER
-  elseif levelNumber > _MAX_LEVEL_NUMBER then
-    levelNumber = 1
-  end
-
-  local levelName = "assets/levels/level_"..levelNumber..".txt"
-
-  io.input(levelName)
-  local text = io.read("*all")
-
-  local levelWidth = 1
-  local tileData = {}
-  local objects = {}
-  local charTable = {}
-  local y = 1
-  for line in text:gmatch("[^\n]+") do
-      local x = 1
-      local levelRow = {}
-      for c in line:gmatch("[^\n]") do
-        table.insert(levelRow, _parseLevelChar(c, objects, charTable, x, y))
-        x = x + 1
-      end
-      levelWidth = math.max(levelWidth, table.getn(levelRow))
-      table.insert(tileData, levelRow)
-      y = y + 1
-  end
-
-  levelHeight = table.getn(tileData)
-
-  return {tiles=tileData, objects=objects, character = charTable["character"], width=levelWidth, height=levelHeight}
 end
 
 function LevelData:getCell(x, y)
---
+++ scenes/level_parser.lua
@@ -0,0 +1,79 @@
+local LevelParser = {}
+
+local _MAX_LEVEL_NUMBER = 1
+
+function LevelParser.loadLevel(levelNumber)
+  if levelNumber < 1 then
+    levelNumber = _MAX_LEVEL_NUMBER
+  elseif levelNumber > _MAX_LEVEL_NUMBER then
+    levelNumber = 1
+  end
+  local levelName = "assets/levels/level_"..levelNumber..".txt"
+
+  io.input(levelName)
+  local text = io.read("*all")
+
+  levelIterator = string.gmatch(text, "[^-]+---")
+
+  local levelMetadata = LevelParser._parseMetadata(levelIterator())
+
+  local levelWidth = 1
+  local tileData = {}
+  local objects = {}
+  local charTable = {}
+  local y = 1
+  for line in levelIterator():gmatch("[^\n]+") do
+    local x = 1
+    local levelRow = {}
+    for c in line:gmatch("[^\n]") do
+    table.insert(levelRow, LevelParser._parseLevelChar(c, objects, charTable, x, y))
+    x = x + 1
+    end
+    levelWidth = math.max(levelWidth, table.getn(levelRow))
+    table.insert(tileData, levelRow)
+    y = y + 1
+  end
+
+  levelHeight = table.getn(tileData)
+
+  return {tiles=tileData, objects=objects, character = charTable["character"], width=levelWidth, height=levelHeight}
+end
+
+function LevelParser._parseMetadata(metadata_text)
+    -- error(metadata_text)
+end
+
+-- Loads a single tile for a level
+-- Adds a floor under objects and adds objects to the "objects" param
+-- Returns the tile (floor, pit, wall)
+ function LevelParser._parseLevelChar(c, objects, charTable, x, y)
+    if c == "b" then
+      objects[x..","..y] = Bone(x, y)
+      return Floor()
+    elseif c == "s" then
+      local character = Character(x, y)
+      charTable["character"] = character
+      objects[x..","..y] = character
+      return Floor()
+    elseif c == "f" then
+      objects[x..","..y] = Fire(x, y)
+      return Floor()
+    elseif c == "." then
+      return Floor()
+    elseif c == "m" then
+      objects[x..","..y] = MetalCrate(x, y)
+      return Floor()
+    elseif c == "w" then
+      objects[x..","..y] = WoodenCrate(x, y)
+      return Floor()
+    elseif c == "x" then
+      return Wall()
+    elseif c == "e" then
+      objects[x..","..y] = Exit(x, y)
+      return Floor()
+    end
+  
+    error("Unable to parse level char "..c)
+end
+
+return LevelParser
